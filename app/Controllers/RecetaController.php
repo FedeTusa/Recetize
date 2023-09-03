c15f907e2f45be7892ee3fe836ca92953743914f
FedeTusa <fedetusa01@gmail.com>
2023-09-03 15:09:48 -0300

    agrego post de receta

---
+++ app/Controllers/RecetaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+use App\Models\RecetaModel;
+
+
+class RecetaController extends BaseController
+{
+    public function new()
+    {
+       return view('receta/new');
+    }
+
+    public function create()
+    {
+       $receta = new RecetaModel();
+       $receta->insert(
+           [
+                'nroReceta' => $this->request->getPost('nroReceta'),
+                'fechaEmision' => $this->request->getPost('fechaEmision'),
+                'Remedio_codigo' => $this->request->getPost('Remedio_codigo'),
+                'Paciente_dni' => $this->request->getPost('Paciente_dni'),
+                'Medico_matricula' => $this->request->getPost('Medico_matricula'),
+            ]
+        );
+        
+       return "se hizo el post"; 
+    }
+}
+
