66bd3fb73b41a94b69fe0f8eef65d82ba2c536c7
FedeTusa <fedetusa01@gmail.com>
2023-11-14 12:38:23 -0300

    conexion con la api de remedio, paciente y medico

--- app/Models/MedicoModel.php
+++ app/Models/MedicoModel.php
@@ -10,4 +10,28 @@ class MedicoModel extends Model
     protected $primaryKey       = 'matricula';
     protected $allowedFields    = ['matricula', 'nombre', 'apellido', 'especialidad', 'localidad'];
 
+    private function sendRequest($method, $url, $data = [])
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        if ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        }
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $response;
+    }
+
+
+    public function crearMedico($data)
+    {
+        return $this->sendRequest('POST', 'http://localhost:8000/medico', $data);
+    }
+
 }
--- app/Models/PacienteModel.php
+++ app/Models/PacienteModel.php
@@ -10,4 +10,28 @@ class PacienteModel extends Model
     protected $primaryKey       = 'dni';
     protected $allowedFields    = ['dni', 'nombre', 'apellido', 'celular', 'localidad', 'calle', 'altura'];
 
+    private function sendRequest($method, $url, $data = [])
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        if ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        }
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $response;
+    }
+
+
+    public function crearPaciente($data)
+    {
+        return $this->sendRequest('POST', 'http://localhost:8000/paciente', $data);
+    }
+
 }
